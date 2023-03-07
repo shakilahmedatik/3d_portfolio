73e4a130814555dba46a98524ddeb2016ae30868
Shakil Ahmed Atik <shakilatik@gmail.com>
2023-03-07 14:25:49 +0600

    root wrapper comp added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,34 @@
+import { BrowserRouter } from 'react-router-dom'
+import {
+  About,
+  Contact,
+  Tech,
+  Experience,
+  Feedbacks,
+  Hero,
+  Navbar,
+  Works,
+  StarsCanvas,
+} from './components'
 const App = () => {
   return (
-    <div className='App'>
-      <h1>3D Dev Portfolio</h1>
-    </div>
+    <BrowserRouter>
+      <div className='relative z-0 bg-primary'>
+        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className='relative z-0'>
+          <Contact />
+          <StarsCanvas />
+        </div>
+      </div>
+    </BrowserRouter>
   )
 }
 
